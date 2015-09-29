dcd44b88d85f539ede3e6bf2ff68252cd609b047
Tobias Conradi <github@tobias-conradi.de>
2015-09-29 18:31:49 +0200

    rdar://22897614 UISlider does not respect semanticContentAttribute value (RTL language support)

---
+++ UISliderRTL/README.md
@@ -0,0 +1,8 @@
+22897614 - UISlider does not respect semanticContentAttribute value (RTL language support)
+=========================================================
+
+rdar://22897614
+http://openradar.appspot.com/22897614
+
+![Screenshot](https://raw.githubusercontent.com/toco/radars/master/UISliderRTL/Screen%20Shot%202015-09-29%20at%2018.08.51.png)
+![Screenshot](https://raw.githubusercontent.com/toco/radars/master/UISliderRTL/Screen%20Shot%202015-09-29%20at%2018.09.10.png)
---
+++ UISliderRTL/UISliderRTL/UISliderRTL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISliderRTL
+//
+//  Created by Tobias Conradi on 29.09.15.
+//  Copyright © 2015 Tobias Conradi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
